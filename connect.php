9154171decf8f78950eb8b4abf127a36f39e8e20
SucubusJang <6306021411010@fitm.kmutnb.ac.th>
2021-03-10 19:27:07 +0700

    create connect.php

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+    define("username", "root");
+    define("password","");
+    define("host" ,"localhost");
+    define("db","iti_shopping");
+    $mysql = new mysqli(host,username,password,db);
+
+?>
