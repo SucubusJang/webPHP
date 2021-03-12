6fd07661a54a01b9f36f9a8dba34a3c1e2039071
kenk4282 <6306021422186@fitm.kmutnb.ac.th>
2021-03-12 11:33:48 +0700

    create rest.php

--- restful/rest.php
+++ restful/rest.php
@@ -1,17 +1,23 @@
 <?php
     include_once("dbcontrol.php");
-    $mydb = new db("sucubus","","test",false);
+    include_once("util.php");
+    $debug_mode = true;
+    
 
-    if($_SERVER['REQUEST_METHOD' == 'post']){
-        
-    }else if($_SERVER['REQUEST_METHOD' == 'get']){
-       
+    if($_SERVER['REQUEST_METHOD'] == 'GET'){ # select
+        debug_text("for GET Method" ,$debug_mode); 
+        show_data();
+    }else if($_SERVER['REQUEST_METHOD'] == 'POST'){ # insert , update , delete
+        debug_text("for POST Method", $debug_mode);
     }else{
+        debug_text("Error Unknow this Request" ,$debug_mode);
         http_response_code(405);
     }
 
     function show_data(){
-        $mydb->query("SELECT * FROM customer");
+        $mydb = new db("sucubus","","test",false);
+        echo json_encode($mydb->query("SELECT * FROM customer"));
+        $mydb->close();
         
     }
 ?>
