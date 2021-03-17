2af173aa14c578a3635056a53187fbe7ecd86499
Your Name <you@example.com>
2021-03-17 17:44:44 +0700

    modify

--- restful/restful.php
+++ restful/restful.php
@@ -14,6 +14,9 @@
             let xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
                 console.log(this.readyState+", ", this.status);
+                if(this.readyState == 4 && this.status == 200){
+                    console.log(this.responseText);
+                }
             }
             xhttp.open("GET", "rest.php", true);
             xhttp.send();
