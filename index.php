e43c6a79e48f896eafb9e9396dd21fd6ab5abc31
Your Name <you@example.com>
2021-03-17 20:08:37 +0700

    M

--- index.php
+++ index.php
@@ -64,7 +64,7 @@
         function load_movie_content() {
             // var n = document.getElementById("Movie_Title");
             // var y = document.getElementById("sel_year");
-            document.getElementById("name").appendChild = "Hello Dolly.";
+            document.getElementById("name").value = "Hello Dolly.";
             // vdocument.getElementById("year");
             // name.value = n.value;
             // year.value = y.value; 
