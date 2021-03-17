dd3018753f534ef3ac462e27d2c1707522e3fc43
Your Name <you@example.com>
2021-03-17 17:26:37 +0700

    modify

--- restful/rest.php
+++ restful/rest.php
@@ -6,7 +6,7 @@
     if($_SERVER['REQUEST_METHOD'] == 'GET'){ # select
         debug_text("for GET Method" ,$debug_mode); 
         show_data($debug_mode);
-    }else if($_SERVER['REQUEST_METHOD'] == 'POST'){ # insert , update , delete
+    }else if($_SERVER['REQUEST_METHOD'] == 'POST'){ # insert , update
         debug_text("for POST Method", $debug_mode);
     }else{
         debug_text("Error Unknow this Request" ,$debug_mode);
