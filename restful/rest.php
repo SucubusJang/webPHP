be6ae398528732a9208547e0cc2bab5b1f4983c0
kenk4282 <6306021422186@fitm.kmutnb.ac.th>
2021-03-19 10:44:46 +0700

    M

--- restful/rest.php
+++ restful/rest.php
@@ -6,8 +6,13 @@
     if($_SERVER['REQUEST_METHOD'] == 'GET'){ # select
         debug_text("for GET Method" ,$debug_mode); 
         echo json_encode(show_data($debug_mode));
-    }else if($_SERVER['REQUEST_METHOD'] == 'POST'){ # insert , update
+    }else if($_SERVER['REQUEST_METHOD'] == 'POST'){ # insert , update //ถ้ามีตัวแปรเข้ามาจะทำการเช็ค
         debug_text("for POST Method", $debug_mode);
+        if(isset($_POST['u_id']) && isset($_POST['u_name'])){
+            $u_id = $_POST['u_id'];
+            $u_name = $_POST['u_name'];
+            insert_data($u_id,$u_name,$debug_mode);
+        }
         $message = '{"Status":'+print_r($_POST)+'}';
         echo json_encode($message);
     }else{
@@ -18,6 +23,13 @@
     function show_data($debug_mode){
         $mydb = new db("root","","personal",$debug_mode);
         $data = $mydb->query("SELECT * FROM customer");
+        return $data;
+    }
+
+    function insert_data($u_id ,$u_name ,$debug_mode){
+        $mydb = new db("root","","personal",$debug_mode);
+        $data = $mydb->query("insert.....");
+        show_data($debug_mode);
         return $data;
     }
 ?>
--- restful/restful.php
+++ restful/restful.php
@@ -9,7 +9,10 @@
 </head>
 
 <body onload="loadContent()">
-    <input type="text" name="" id="u_id"> <input type="text" name="" id="u_name">
+    <label>ID:</label>
+    <input type="text" name="" id="u_id"> 
+    <label>Name:</label>
+    <input type="text" name="" id="u_name">
     <button onclick="add_new()">Click</button><br>
     <div id="out"></div>
     <script>
@@ -40,7 +43,18 @@
             // xhttp.send("a=12&b=15");
         }
         function add_new(){
-
+            let xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if(this.readyState == 4 &&  this.status == 200){
+                    alert(this.responseText);
+                }
+            }
+            //-------------------------------------------------------------------------------
+            xhttp.open("POST", "rest.php", true); // การส่งค่าไปหน้า rest.php
+            xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            u_id = document.getElementById("u_id"); // นำค่ามาใส่ให้ตัวแปร u_id
+            u_name = document.getElementById("u_name"); // นำค่ามาใส่ให้ตัวแปร u_name
+            xhttp.send("u_id="+u_id.value+"&u_name="+u_name.value); // ส่งค่า u_id และ u_name โดยถ้าส่งมากกว่า 1 ค่า ให้เชื่อมด้วย &
         }
     </script>
 </body>
