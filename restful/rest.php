729cb12beda6c29761ad48feccf5fcb01e1dd2d3
kenk4282 <6306021422186@fitm.kmutnb.ac.th>
2021-03-19 09:39:50 +0700

    M

--- restful/rest.php
+++ restful/rest.php
@@ -5,9 +5,11 @@
     
     if($_SERVER['REQUEST_METHOD'] == 'GET'){ # select
         debug_text("for GET Method" ,$debug_mode); 
-        show_data($debug_mode);
+        echo json_encode(show_data($debug_mode));
     }else if($_SERVER['REQUEST_METHOD'] == 'POST'){ # insert , update
         debug_text("for POST Method", $debug_mode);
+        $message = '{"Status":"Success","Message":'+$_POST+'}';
+        echo json_encode($message);
     }else{
         debug_text("Error Unknow this Request" ,$debug_mode);
         http_response_code(405);
@@ -15,7 +17,7 @@
 
     function show_data($debug_mode){
         $mydb = new db("root","","personal",$debug_mode);
-        echo json_encode($mydb->query("SELECT * FROM customer"));
-        $mydb->close();
+        $data = $mydb->query("SELECT * FROM customer");
+        return $data;
     }
 ?>
